594ba28a654092c69b20d3f29b61b0616af2dd1c
Sérgio Gomes <srochg@gmail.com>
2022-06-23 12:23:30 -0300

    Impnantando alteraçoes no file service

--- nfe-process-service/src/main/java/com/daniele/nfe/config/ConnectionRabbitMQ.java
+++ nfe-process-service/src/main/java/com/daniele/nfe/config/ConnectionRabbitMQ.java
@@ -2,25 +2,30 @@ package com.daniele.nfe.config;
 
 import com.rabbitmq.client.ConnectionFactory;
 
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ConnectionRabbitMQ {
 
-    @Configuration
-    public class CamelConfiguration {
 
+    @Value("${rabbitmq.address}")
+    private String address;
 
+    @Value("${rabbitmq.port}")
+    private Integer port;
 
-        @Bean
-        public ConnectionFactory rabbitConnectionFactoryFirst() {
-            ConnectionFactory factory = new ConnectionFactory();
-            factory.setHost("127.1.0.0");
-            factory.setPort(5672);
-            factory.setUsername("rabbitmq");
-            factory.setPassword("rabbitmq");
-            return factory;
-        }
+    @Bean
+    public ConnectionFactory rabbitConnectionFactoryFirst() {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(this.address);
+        factory.setPort(this.port);
+        factory.setUsername("rabbitmq");
+        factory.setPassword("rabbitmq");
+        return factory;
     }
+
+
 }
